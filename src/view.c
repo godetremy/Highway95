a884ad9d2abbe61e400f971131865a6518e751cc
godetremy <godetremy@free.fr>
2025-03-17 22:08:04 +0100

    fix: Invalid name causing compilation fail on MacOS

--- src/view.c
+++ src/view.c
@@ -6,7 +6,7 @@
 /*   By: rgodet <rgodet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 17:14:35 by rgodet            #+#    #+#             */
-/*   Updated: 2025/03/17 19:48:32 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/03/17 22:06:39 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ void	view_menu(t_game *game)
 void	view_pause(t_game *game)
 {
 	SDL_SetRenderDrawColor(game->renderer, HIGH_R, HIGH_G, HIGH_B, 0);
-	draw_str(pause, game, 22, 10);
-	draw_str(resume, game, 27, 25);
+	draw_str(pause_text, game, 22, 10);
+	draw_str(resume_text, game, 27, 25);
 	draw_str(exit_text, game, 32, 35);
 	draw_str(arrow, game, 22, 25 + (game->menu_select * 10));
 }
