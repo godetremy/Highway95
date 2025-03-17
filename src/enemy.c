48df483e5f4870ebbf3213a5dbeacdffad225b6a
rgodet <marvin@student.42.fr>
2025-03-17 18:04:32 +0100

    feat: Add multiple enemy car textures

--- src/enemy.c
+++ src/enemy.c
@@ -6,12 +6,27 @@
 /*   By: rgodet <rgodet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 17:28:57 by rgodet            #+#    #+#             */
-/*   Updated: 2025/03/17 10:28:55 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/03/17 18:00:07 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/game.h"
 #include <stdlib.h>
+
+static char	*randomize_texture()
+{
+	char	*texture;
+	int	random;
+
+	random = rand() % 3;
+	if (random == 0)
+		texture = car1;
+	else if (random == 1)
+		texture = car2;
+	else
+		texture = car3;
+	return (texture);
+}
 
 t_enemy	enemy_init(int spawn_distance)
 {
@@ -19,6 +34,9 @@ t_enemy	enemy_init(int spawn_distance)
 
 	enemy.car_disposition = rand() % 6;
 	enemy.x = 84 + spawn_distance;
+	enemy.car1_str = randomize_texture();
+	enemy.car2_str = randomize_texture();
+	enemy.car3_str = randomize_texture();
 	return (enemy);
 }
 
@@ -39,19 +57,22 @@ static void	relocate_enemy(t_game *game, t_enemy *enemy)
 		game->spawn_distance -= 1;
 	enemy->x = game->spawn_distance + spawn_distance;
 	enemy->car_disposition = rand() % 6;
+	enemy->car1_str = randomize_texture();
+	enemy->car2_str = randomize_texture();
+	enemy->car3_str = randomize_texture();
 }
 
 void	draw_enemy(t_game *game, t_enemy *enemy)
 {
 	if (enemy->car_disposition == 0 || enemy->car_disposition == 3
 		|| enemy->car_disposition == 5)
-		draw_str(car1, game, enemy->x, 7);
+		draw_str(enemy->car1_str, game, enemy->x, 7);
 	if (enemy->car_disposition == 1 || enemy->car_disposition == 3
 		|| enemy->car_disposition == 4)
-		draw_str(car1, game, enemy->x, 7 + 14);
+		draw_str(enemy->car2_str, game, enemy->x, 7 + 14);
 	if (enemy->car_disposition == 2 || enemy->car_disposition == 4
 		|| enemy->car_disposition == 5)
-		draw_str(car1, game, enemy->x, 7 + 14 * 2);
+		draw_str(enemy->car3_str, game, enemy->x, 7 + 14 * 2);
 	enemy->x -= 1;
 	if (enemy->x < -20)
 	{
