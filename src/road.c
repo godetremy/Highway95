0dc5c7af76aa419ebc10f81679f118efb564d5d0
rgodet <marvin@student.42.fr>
2025-03-17 09:20:32 +0100

    feat: Incremented road speed

--- src/road.c
+++ src/road.c
@@ -6,7 +6,7 @@
 /*   By: rgodet <rgodet@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 11:31:36 by rgodet            #+#    #+#             */
-/*   Updated: 2025/03/16 17:20:32 by rgodet           ###   ########.fr       */
+/*   Updated: 2025/03/17 09:14:40 by rgodet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void draw_road(t_game *game)
 	int			i;
 	int			shift;
 
-	shift = game->frame % 8;
+	shift = game->frame % 6;
 	SDL_SetRenderDrawColor(game->renderer, COLOR_LOW_R, COLOR_LOW_G, COLOR_LOW_B, 0);
 	i = 0;
 	while (i < 12)
 	{
-		rect = set_rect((8 * i) - shift, 20, 6, 1);
+		rect = set_rect((8 * i) - shift * 1.5, 20, 6, 1);
 		SDL_RenderFillRect(game->renderer, &rect);
 		i++;
 	}
 	i = 0;
 	while (i < 12)
 	{
-		rect = set_rect((8 * i) - shift, 34, 6, 1);
+		rect = set_rect((8 * i) - shift * 1.5, 34, 6, 1);
 		SDL_RenderFillRect(game->renderer, &rect);
 		i++;
 	}
